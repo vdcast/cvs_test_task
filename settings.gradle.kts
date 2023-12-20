dbebfaa470c5ea87fda1efba56862f33e56b881a
VD <vdfree448@protonmail.com>
2023-12-20 10:45:18 +0200

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "cvs_test_task"
+include(":app")
+ 
